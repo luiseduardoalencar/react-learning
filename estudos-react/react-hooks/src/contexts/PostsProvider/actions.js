c550da6af2936ccbc90ef373e8a1e6786e5fdbf0
Luis Eduardo Alencar Melo <luiseduardoalencarmelo@gmail.com>
2023-10-10 08:52:17 -0300

    'initial'

---
+++ estudos-react/react-hooks/src/contexts/PostsProvider/actions.js
@@ -0,0 +1,10 @@
+import * as types from './types';
+
+export const loadPosts = async (dispatch) => {
+  dispatch({ type: types.POSTS_LOADING });
+
+  const postsRaw = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await postsRaw.json();
+  console.log('Carreguei os posts');
+  dispatch({ type: types.POSTS_SUCCESS, payload: posts });
+};
